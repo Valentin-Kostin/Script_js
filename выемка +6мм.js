8c64a066a39e0466a1ef9c93100e30a917b547eb
Valentin Kostin <frezer.2021@mail.ru>
2026-08-11 10:23:07 +0300

    poluchilos s koordinatami 2

--- выемка +6мм.js
+++ выемка +6мм.js
@@ -13,35 +13,43 @@ if (!panel) {
 } else {
     // Формируем сообщение с координатами панели
     var panelMsg = "Панель:\n";
+    var shirina_P_Min = panel.GMin.x;
+    var shirina_P_Max = panel.GMax.x;
+    var dlina_P_Min = panel.GMin.y;
+    var dlina_P_Max = panel.GMax.y;
+    var Tolchina = panel.GSize.z;
     panelMsg += "Ширина (GSize.x): " + panel.GSize.x + " мм\n";
     panelMsg += "Длина (GSize.y): " + panel.GSize.y + " мм\n";
     panelMsg += "Толщина (GSize.z): " + panel.GSize.z + " мм\n";
-    alert(panelMsg);
-    panelMsg += "Min X: " + panel.GMin.x + ", Max X: " + panel.GMax.x + "\n";
-    panelMsg += "Min Y: " + panel.GMin.y + ", Max Y: " + panel.GMax.y + "\n";
-    panelMsg += "Min Z: " + panel.GMin.z + ", Max Z: " + panel.GMax.z + "\n";
+    panelMsg += "Min X: " + shirina_P_Min + ", Max X: " + shirina_P_Max + "\n";
+    panelMsg += "Min Y: " + dlina_P_Min + ", Max Y: " + dlina_P_Max + "\n";
     
     alert(panelMsg);
     
     // Считаем и выводим выемки
     var notchCount = 0;
-    var notchesMsg = "Выемки:\n";
+    var pocketMsg = "Выемки:\n";
     
     for (var i = 0; i < panel.Cuts.Count; i++) {
         if (panel.Cuts[i].CutType == 2) { // Проверяем только выемки
             notchCount++;
             var cut = panel.Cuts[i];
             
-            notchesMsg += "\nВыемка #" + (i + 1) + ":\n";
-            notchesMsg += "Min X: " + cut.Contour.Min.x.toFixed(2) + ", Max X: " + cut.Contour.Max.x.toFixed(2) + "\n";
-            notchesMsg += "Min Y: " + cut.Contour.Min.y.toFixed(2) + ", Max Y: " + cut.Contour.Max.y.toFixed(2) + "\n";
+            pocketMsg += "\nВыемка #" + (i + 1) + ":\n";
+            var pocked_Min_x = cut.Contour.Min.x.toFixed(2);
+            var pocked_Max_x = cut.Contour.Max.x.toFixed(2);
+            var pocked_Min_y = cut.Contour.Min.y.toFixed(2);
+            var pocked_Max_y = cut.Contour.Max.y.toFixed(2);
+
+            pocketMsg += "Min X: " + pocked_Min_x + ", Max X: " + pocked_Max_x + "\n";
+            pocketMsg += "Min Y: " + pocked_Min_y + ", Max Y: " + pocked_Max_y + "\n";
             
             // Выводим точки контура
-            notchesMsg += "Точки контура:\n";
+            pocketMsg += "Точки контура:\n";
             for (var t = 0; t < cut.Contour.Count; t++) {
                 var obj = cut.Contour.Objects[t];
                 if (obj == '[object T2DLine]') {
-                    notchesMsg += "  Линия: (" + obj.Pos1.x.toFixed(2) + ", " + obj.Pos1.y.toFixed(2) + ") -> (" + 
+                    pocketMsg += "  Линия: (" + obj.Pos1.x.toFixed(2) + ", " + obj.Pos1.y.toFixed(2) + ") -> (" + 
                                   obj.Pos2.x.toFixed(2) + ", " + obj.Pos2.y.toFixed(2) + ")\n";
                 }
             }
@@ -49,8 +57,8 @@ if (!panel) {
     }
     
     if (notchCount === 0) {
-        notchesMsg += "Выемки не найдены.";
+        pocketMsg += "Выемки не найдены.";
     }
     
-    alert(panelMsg+notchesMsg);
+    alert(panelMsg+pocketMsg);
 }
